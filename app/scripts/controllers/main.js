a69fde19bbb5c95efdd127a6cb995c85021d8f8d
joalmay <joalmay@gmail.com>
2014-02-27 19:58:59 -0700

    step1

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('realTimeForumApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
